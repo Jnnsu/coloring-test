892b0bfd201157e8159a49c63fdc53a3dcb8e3de
Jnnsu <jspp120@gmail.com>
2024-03-10 02:56:10 +0900

    로그인 반응형

--- src/components/UserLogin/GenderButtons/index.tsx
+++ src/components/UserLogin/GenderButtons/index.tsx
@@ -6,13 +6,13 @@ export const GenderButtons = () => {
       <div className={styles.genderButton}>
         <label htmlFor="gender-male" className={styles.genderButtonInput}>
           <input type="radio" id="gender-male" name="gender" value="male" />
-          <span>남성</span>
+          남성
         </label>
       </div>
       <div className={styles.genderButton}>
         <label htmlFor="gender-female" className={styles.genderButtonInput}>
           <input type="radio" id="gender-female" name="gender" value="female" />
-          <span>여성</span>
+          여성
         </label>
       </div>
     </div>
